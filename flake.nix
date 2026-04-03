9fbb765debec5033af0dcfe15409a06967617965
Peter Bittner <peter@painless.software>
2026-04-04 00:08:50 +0200

    Add Nix flake to provide development shell

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+  outputs = { nixpkgs, ... }:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+    in {
+      devShells = forEachSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            packages = [ pkgs.google-cloud-sdk pkgs.uv ];
+            shellHook = ''
+              uv python install 3.14 --quiet
+              uv tool install rust-just --quiet
+            '';
+          };
+        });
+    };
+}
